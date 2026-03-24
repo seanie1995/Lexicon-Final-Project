32d2a3260bb0ac7787f1a3ef1f003eea93099823
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2026-03-24 11:17:18 +0100

    Created interfaces for Product, category, condition, author and publisher

---
+++ apps/webshop/src/app/types/types.ts
@@ -0,0 +1,48 @@
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  stock: number;
+  weight: number;
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  category: Category;
+  images: string[];
+  thumbnail: string;
+  tags: string[];
+  condition: Condition;
+  era: string;
+  genre: string;
+  author: Author;
+  format: string;
+  publisher: Publisher;
+  year: Date;
+  binding: string;
+}
+
+interface Category {
+  id: number;
+  name?: string;
+}
+
+interface Condition {
+  id: number;
+  exterior: string;
+  interior: string;
+  grade: string;
+}
+
+interface Author {
+  id: number;
+  name: string;
+  description: string;
+}
+
+interface Publisher {
+  id: number;
+  name: string;
+  description: string;
+}

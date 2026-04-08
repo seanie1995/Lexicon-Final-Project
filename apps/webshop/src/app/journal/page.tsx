4d986c5016fabbc54a475af792738e909c166206
Emelie Nyberg <emelie.kedert@gmail.com>
2026-04-08 13:31:14 +0200

    feat: add journal page and hero component

---
+++ apps/webshop/src/app/journal/page.tsx
@@ -0,0 +1,9 @@
+import JournalHero from "@/components/journal-hero";
+
+export default function Journal() {
+    return (
+        <main className="pt-24">
+            <JournalHero />
+        </main>
+    );
+}
